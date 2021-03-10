d4e444364b26ba3c3b324944c0eea40397e22feb
cthouse <cthouse13@gmail.com>
2021-03-10 14:14:16 -0700

    Pregame Update commit

--- pregame.py
+++ pregame.py
@@ -6,10 +6,6 @@ def pregame(gameid):
     pregame=True
     game_response = requests.get('https://statsapi.web.nhl.com/api/v1/game/'+gameid+'/feed/live')
     game=json.loads(game_response.text)
-    sec_gametime=time.mktime(time.strptime(game['gameData']['datetime']['dateTime'],"%Y-%m-%dT%H:%M:%SZ"))-time.mktime(time.gmtime())
-    hours_gametime=int(sec_gametime/3600)
-    mins_gametime=int((sec_gametime%3600)/60)
-    secs_gametime=int((sec_gametime%3600)%60)
     print("{} at {} ".format(game['gameData']['teams']['away']['name'],game['gameData']['teams']['home']['name']))
     while pregame:
         game_response = requests.get('https://statsapi.web.nhl.com/api/v1/game/'+gameid+'/feed/live')
